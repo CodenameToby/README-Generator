f8c22b3d4a1fd03ba1c3b5d4de4ecf8406df8776
Toby Warburton <tobywarburton10@gmail.com>
2023-07-20 10:57:58 -0700

    added function to prompt user for required info

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-var inquirer = require('inquirer');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// prompt function from inquirer to get info from user
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your Repository: ',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a decription of your project: ',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contributing guidelines',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter testing instructions',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your application:',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter your GitHub username: ',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        
+    ]);
+}
+
+// function to generate README.md from user input using template literals
